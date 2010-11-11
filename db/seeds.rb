ce13a64763d55186c4922a71593e6af29a5c1224
Timur Vafin <timur.vafin@flatsoft.com>
2010-11-11 14:30:52 +0300

    Create default plans from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+%w{Free Basic Premium}.each do |plan_name|
+  SubscriptionPlan.create(:name => plan_name)
+end
