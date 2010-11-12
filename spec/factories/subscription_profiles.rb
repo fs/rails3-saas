b6eb782e0d7a7f029134de53e04dee70fc9ec8f2
Timur Vafin <timur.vafin@flatsoft.com>
2010-11-12 13:29:24 +0300

    USe singular factory name

--- spec/factories/subscription_profiles.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-Factory.define :subscription_profile do |f|
-  f.subscription_id 1
-  f.payment_auth_id "MyString"
-  f.card_number "MyString"
-  f.card_expired_on "2010-11-11"
-  f.card_holder_name "MyString"
-end
