999cbc57c121e65c95da7c203f27284c60c24bc5
Timur Vafin <timur.vafin@flatsoft.com>
2010-11-12 15:10:53 +0300

    Added gateway config and active merchant initializer

---
+++ config/initializers/active_merchant.rb
@@ -0,0 +1,5 @@
+configatron.gateway.configure_from_hash(YAML.load(File.read("#{Rails.root}/config/gateway.yml"))[Rails.env])
+
+ActiveMerchant::Billing::Base.mode = configatron.gateway.mode = :test if configatron.gateway.test?
+
+configatron.gateway.current = ActiveMerchant::Billing::Base.gateway(configatron.gateway.name).new(configatron.gateway.options)
