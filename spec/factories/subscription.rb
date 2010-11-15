673260d7b5103ba502be0017be8eb3db02de2fad
Timur Vafin <timur.vafin@flatsoft.com>
2010-11-15 19:58:52 +0300

    Added profile to the subscription factory

--- spec/factories/subscription.rb
+++ spec/factories/subscription.rb
@@ -1,4 +1,5 @@
 Factory.define :subscription do |f|
   f.association :account
   f.association :plan, :factory => :free_subscription_plan
+  f.association :profile, :factory => :subscription_profile
 end
