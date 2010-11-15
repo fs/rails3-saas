0de9ffe4d0190b58ee498d8e1dcdebf8d182bc76
Timur Vafin <timur.vafin@flatsoft.com>
2010-11-15 19:58:35 +0300

    Genarate subscription profile with real gateway data

--- spec/factories/subscription_profile.rb
+++ spec/factories/subscription_profile.rb
@@ -1,8 +1,3 @@
 Factory.define :subscription_profile do |f|
-  f.association :subscription
-  f.payment_auth_id 1
-  f.card_number 'XXXX-XXXX-XXXX-4338'
-  f.card_type 'Visa'
-  f.card_expired_on 1.year.from_now
-  f.card_holder_name 'John Smith'
+  f.card Factory(:credit_card)
 end
