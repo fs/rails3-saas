e0f548493278f4924f63fa3cd0fb39f65b056b64
Ildar Manzhikov <ildar.manzhikov@flatsoft.com>
2010-11-19 19:45:59 +0300

    Returned full_name attr_accessible, and method full_name_with_email to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,5 @@ describe User do
   it { should allow_mass_assignment_of(:email) }
   it { should allow_mass_assignment_of(:password) }
   it { should allow_mass_assignment_of(:password_confirmation) }
-
-  it { should validate_presence_of :full_name }
-
   it { should belong_to :account }
 end
