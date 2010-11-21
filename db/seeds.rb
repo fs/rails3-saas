fbdad5857f9479817d2b1baf0c7e3b9e34d7888c
Timur Vafin <timur.vafin@flatsoft.com>
2010-11-21 19:33:03 +0300

    [#5934986] Added plans to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-%w{Free Basic Premium}.each do |plan_name|
-  SubscriptionPlan.create(:name => plan_name)
-end
+SubscriptionPlan.create(:name => 'Free', :amount => 0, :user_limit => 2)
+SubscriptionPlan.create(:name => 'Basic', :amount => 10, :user_limit => 5)
+SubscriptionPlan.create(:name => 'Premium', :amount => 50, :user_limit => 10)
